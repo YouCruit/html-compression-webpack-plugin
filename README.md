b66a51474f50a5337ce934bb53c15c0e4f19a02e
Anon <tom.debacker@outlook.com>
2016-12-08 11:22:40 +0100

    New features

--- README.md
+++ README.md
@@ -1,4 +1,8 @@
-# html compression plugin for webpack
+# HTML compression plugin for webpack
+
+## Info
+
+The `compression-webpack-plugin` didn't compress HTML files. This plugin aims to solve that. Also added an option to remove the original assets after compressing.
 
 ## Usage
 
@@ -10,21 +14,28 @@ var HTMLCompressionPlugin = require("html-compression-webpack-plugin");
 module.exports = {
 	plugins: [
 		new HTMLCompressionPlugin({
-			asset: "[path].gz[query]",
-			algorithm: "gzip",
-			test: /\.js$|\.html$/,
-			threshold: 10240,
-			minRatio: 0.8
-		})
+			testHTML: /\.html$/,
+			test: /.*\.(css|js)$/i,
+			deleteOriginals: true,
+			assetsRelativeOutputDirectory: '../build/client/assets',
+			asset: '[path].gz',
+			algorithm: 'gzip',
+			threshold: 0,
+			minRatio: 0.0
+	})
 	]
 }
 ```
 
+
 Arguments:
 
-* `asset`: The target asset name. `[file]` is replaced with the original asset. `[path]` is replaced with the path of the original asset and `[query]` with the query. Defaults to `"[path].gz[query]"`.
+* `asset`: The target asset name. `[file]` is replaced with the original asset. `[path]` is replaced with the path of the original asset and `[query]` with the query. Defaults to `"[path].gz[query]"`
+* `assetsRelativeOutputDirectory`: The relative directory where your assets will be output to. Plugin will look in sub-folders for the original assets aswell.
 * `algorithm`: Can be a `function(buf, callback)` or a string. For a string the algorithm is taken from `zlib` (or zopfli for `zopfli`). Defaults to `"gzip"`.
-* `test`: All assets matching this RegExp are processed. Defaults to every HTML asset.
+* `deleteOriginals`: All original assets which have been compressed will be deleted in the done phase. The stats after bundling will show the files emitted but they will be deleted. Defaults to `true`
+* `test`: All assets matching this RegExp are processed. Defaults to `/.*\.(js|css)$/i`
+* `testHTML`: All assets matching this RegExp are processed. This will happen in the emit phase. Defaults to `/\.html$/`
 * `threshold`: Only assets bigger than this size are processed. In bytes. Defaults to `0`.
 * `minRatio`: Only assets that compress better that this ratio are processed. Defaults to `0.8`.
 
--- index.js
+++ index.js
@@ -6,6 +6,8 @@ Co-author Tom De Backer @TomDeBacker
 
 var async = require("async");
 var url = require('url');
+var fs = require('fs');
+var path = require('path');
 
 var RawSource = require("webpack-sources/lib/RawSource");
 
@@ -47,23 +49,29 @@ function HTMLCompressionPlugin(options) {
 			};
 		}
 	}
-	this.test = options.test || options.regExp || /\.html$/
+	this.testHTML = options.testHTML ||  /\.html$/
+	this.test = options.test || /.*\.(js|css)$/i
 	this.threshold = options.threshold || 0;
 	this.minRatio = options.minRatio || 0.8;
+	this.deleteOriginals = options.deleteOriginals || true
+	this.assetsRelativeOutputDirectory = options.assetsRelativeOutputDirectory
+	this.originalAssetsPaths = new Array();
+	if(!this.assetsRelativeOutputDirectory && this.deleteOriginals) 
+		throw new Error('Set relative output directory of assets when enabling deletion of original files');
 }
 module.exports = HTMLCompressionPlugin;
 
 HTMLCompressionPlugin.prototype.apply = function(compiler) {
-	compiler.plugin('emit', function(compilation, callback) {
-		async.forEach(Object.keys(compilation.assets), function(file, callback) {
+	compiler.plugin("this-compilation", function(compilation) {
+		compilation.plugin('optimize-assets', function(assets, callback) {
+			async.forEach(Object.keys(assets), function(file, callback) {
 				if(Array.isArray(this.test)) {
 					if(this.test.every(function(t) {
 						return !t.test(file);
 					})) return callback();
 				} else if(this.test && !this.test.test(file))
-					return callback();
-				console.log(file + " is HTML!")
-				var asset = compilation.assets[file];
+				return callback();
+				var asset = assets[file];
 				var content = asset.source();
 				if(!Buffer.isBuffer(content))
 					content = new Buffer(content, "utf-8");
@@ -81,9 +89,52 @@ HTMLCompressionPlugin.prototype.apply = function(compiler) {
 					var newFile = this.asset.replace(/\[(file|path|query)\]/g, function(p0,p1) {
 						return sub[p1];
 					});
-					compilation.assets[newFile] = new RawSource(result);
+					if(this.deleteOriginals === true){
+						this.originalAssetsPaths.push(path.join(__dirname, this.assetsRelativeOutputDirectory, file));
+					}
+					assets[newFile] = new RawSource(result);
 					callback();
 				}.bind(this));
+			}.bind(this), callback);
+		}.bind(this));
+	}.bind(this));
+	compiler.plugin('emit', function(compilation, callback) {
+		async.forEach(Object.keys(compilation.assets), function(file, callback) {
+			if(Array.isArray(this.testHTML)) {
+				if(this.testHTML.every(function(t) {
+					return !t.test(file);
+				})) return callback();
+			} else if(this.testHTML && !this.testHTML.test(file))
+			return callback();
+			var asset = compilation.assets[file];
+			var content = asset.source();
+			if(!Buffer.isBuffer(content))
+				content = new Buffer(content, "utf-8");
+			var originalSize = content.length;
+			if(originalSize < this.threshold) return callback();
+			this.algorithm(content, this.compressionOptions, function(err, result) {
+				if(err) return callback(err);
+				if(result.length / originalSize > this.minRatio) return callback();
+				var parse = url.parse(file);
+				var sub = {
+					file: file,
+					path: parse.pathname,
+					query: parse.query || ""
+				};
+				var newFile = this.asset.replace(/\[(file|path|query)\]/g, function(p0,p1) {
+					return sub[p1];
+				});
+				if(this.deleteOriginals === true){
+					this.originalAssetsPaths.push(path.join(__dirname, this.assetsRelativeOutputDirectory, file));
+				}
+				compilation.assets[newFile] = new RawSource(result);
+				callback();
+			}.bind(this));
 		}.bind(this), callback);
+	}.bind(this));
+	compiler.plugin('done', function(stats) {
+		for (var i in this.originalAssetsPaths) {
+			fs.unlink(this.originalAssetsPaths[i]);
+		}
 	}.bind(this));
 };
